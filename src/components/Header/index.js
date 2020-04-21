de8dd3b1f4672ba008e112f8ea155c376a116c6f
Wanderson Castro <wandersonce1@hotmail.com>
2020-04-20 23:45:05 -0700

    Just template and assets

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+    return (
+        <Container>
+            <Top>
+                <Logo source={} />
+                <Title>Wanderson</Title>
+            </Top>
+        </Container>
+    );
+}
