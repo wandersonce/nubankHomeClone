24e6acf10db650a17d30aaa66e6f02b09f1a4040
Wanderson Castro <wandersonce1@hotmail.com>
2020-04-21 00:12:55 -0700

    Path and file ajust

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Top, Logo, Title } from './styles';
+import logo from '~/assets/Nubank_Logo.png'
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 export default function Header() {
     return (
         <Container>
             <Top>
-                <Logo source={} />
+                <Logo source={logo} />
                 <Title>Wanderson</Title>
             </Top>
+            <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
         </Container>
     );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,3 +4,20 @@ export const Container = styled.View`
   align-items: center;
   padding: 40px 0 30px;
 `;
+
+export const Top = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+export const Logo = styled.Image`
+
+`;
+
+export const Title = styled.Text`
+    font-size: 18px;
+    color: #fff;
+    font-weight: bold;
+    margin-left: 8px;
+`;
